df4b599f7df64bbc54633fcceb867a83c6d9c65f
fernando <fernandomercado529@gmail.com>
2022-07-26 14:44:54 -0400

    Add Crud Assistant (Has Error)

--- StoreVideoGames/Manager/AssistantM/AssistantManager.cs
+++ StoreVideoGames/Manager/AssistantM/AssistantManager.cs
@@ -1,28 +1,91 @@
-﻿using EFDataAccess.Models;
+﻿using AutoMapper;
+using EFDataAccess.Models;
 using StoreVideoGames.DTOs.AssistantDTOs;
+using StoreVideoGames.Repositories.AssistantRepository;
 
 namespace StoreVideoGames.Manager.AssistantM
 {
     public class AssistantManager : IAssistantManager
     {
-        public Task<ManagerResult<Assistant>> AddAsync(CreateAssistantDTO createAssistantDTO)
+        private readonly IAssistantRepository _repository;
+        private readonly IMapper _mapper;
+
+        public AssistantManager(IAssistantRepository repository, IMapper mapper)
         {
-            throw new NotImplementedException();
+            _repository = repository;
+            _mapper = mapper;
         }
 
-        public Task<ManagerResult<Assistant>> DeleteAsync(int id)
+
+        public async Task<ManagerResult<Assistant>> AddAsync(CreateAssistantDTO createAssistantDTO)
         {
-            throw new NotImplementedException();
+            var managerResult = new ManagerResult<Assistant>();
+            try
+            {
+                var entity = _mapper.Map<Assistant>(createAssistantDTO);
+                await _repository.AddAsync(entity);
+                managerResult.Message = "Add Successfully";
+                return managerResult;
+            }
+            catch (Exception e)
+            {
+                managerResult.Success = false;
+                managerResult.Message = e.Message;
+                return managerResult;
+            }
         }
 
-        public Task<ManagerResult<Assistant>> GetAsync()
+        public async Task<ManagerResult<Assistant>> DeleteAsync(int id)
         {
-            throw new NotImplementedException();
+            var managerResult = new ManagerResult<Assistant>();
+            var AssistantEntity = await _repository.GetByIdAsync(id);
+            if (AssistantEntity == null)
+            {
+                managerResult.Success = false;
+                managerResult.Message = "Not exist Assistant";
+                return managerResult;
+            }
+
+            var entity = _mapper.Map<Assistant>(AssistantEntity);
+            await _repository.DeleteAsync(entity);
+            managerResult.Message = "Delete Successfully";
+            return managerResult;
         }
 
-        public Task<ManagerResult<Assistant>> UpdateAsync(int id, CreateAssistantDTO updateAssistantDTO)
+        public async Task<ManagerResult<Assistant>> GetAsync()
         {
-            throw new NotImplementedException();
+            var managerResult = new ManagerResult<Assistant>();
+            managerResult.Data = await _repository.GetAllAsync();
+            return managerResult;
+        }
+
+        public async Task<ManagerResult<Assistant>> UpdateAsync(int id, CreateAssistantDTO updateAssistantDTO)
+        {
+            var managerResult = new ManagerResult<Assistant>();
+            try
+            {
+                var AssistantEntity = await _repository.GetByIdAsync(id);
+
+                if (AssistantEntity == null)
+                {
+                    managerResult.Success = false;
+                    managerResult.Message = "Not exist Assistant";
+                    return managerResult;
+                }
+                AssistantEntity.isAttend = updateAssistantDTO.isAttend;
+                AssistantEntity.ReservationId = updateAssistantDTO.reservationId;
+
+                //var entity = _mapper.Map<Assistant>(updateAssistantDTO);
+                await _repository.UpdateAsync(AssistantEntity);
+                managerResult.Message = "Update Successfully";
+                return managerResult;
+            }
+            catch (Exception e)
+            {
+                managerResult.Success = false;
+                managerResult.Message = e.Message;
+                return managerResult;
+            }
         }
     }
 }
