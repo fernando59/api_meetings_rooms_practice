65717c1a9fb2a80dd2a72ee42f0dcb1db98077a2
fernando <fernandomercado529@gmail.com>
2022-07-26 13:14:33 -0400

    Add Controllers

---
+++ StoreVideoGames/Controllers/AssistantController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StoreVideoGames.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AssistantController : ControllerBase
+    {
+
+
+    }
+}
